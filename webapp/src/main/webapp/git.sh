f4843ba7d32e5157aa95e28430b307dca7d26714
tchatua <tchattua@gmail.com>
2022-07-27 00:28:04 -0400

    Updated index.jsp file 072720222409

--- webapp/src/main/webapp/git.sh
+++ webapp/src/main/webapp/git.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 git status
+echo "git status"
 sleep 4
 git add .
+echo "git status"
 sleep 4
 git commit -m "Updated index.jsp file 072720222409"
+echo "git commit"
 sleep 4
 git push origin master
+echo "git push origin master/main"
